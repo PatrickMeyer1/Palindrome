bbc01d39f94572e1861aca40060a78039efd99ec
PatrickMeyer1 <meyerpatrick6378@gmail.com>
2022-11-15 14:15:48 -0500

    Commiting palindrome file

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+# Method verifying if a given word is a palindrome or not.
+# A palindrome is a word that can be read the same backward or forwards
+def padlindrome(word):
+    length = len(word)
+    half_length = length//2
+    for i in range(1, half_length + 1):
+        if word[i-1] == word[-i]:
+            print( word[i-1] + " is equal to " + word[-i] )
+            if i == half_length:
+                print("The word is a palindrome")
+        else:
+            print( word[i-1] + " is NOT equal to " + word[-i] )
+            print("Thus, the word is not a palindrome")
+            break
+
+
+# Examples
+
+# 'laval' is a palindrome
+# 'lavval' is a palindrome
+# 'lavaval' is a plaindrome
+# 'laavval' is not a palindrome
